23a00b4646c343d2b900d245fb74bf0ad4441e66
waku25 <yado.kt@me.com>
2018-11-07 11:11:27 +0900

    first commit

---
+++ opticalflow.py
@@ -0,0 +1,58 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture('slow.flv')
+
+# ShiTomasiコーナー検出器のためのパラメータ
+feature_params = dict(maxCorners=100,
+                      qualityLevel=0.3,
+                      minDistance=7,
+                      blockSize=7)
+
+# Lucas-Kanade法によるオプティカル・フローのためのパラメータ
+lk_params = dict(winSize=(15, 15),
+                 maxLevel=2,
+                 criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+
+# ランダムな色の生成
+color = np.random.randint(0, 255, (100, 3))
+
+# 最初のフレームを取り出し、コーナーを求める
+ret, old_frame = cap.read()
+old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
+p0 = cv2.goodFeaturesToTrack(old_gray, mask=None, **feature_params)
+
+# 描画のためマスク画像を生成
+mask = np.zeros_like(old_frame)
+
+while(1):
+    ret, frame = cap.read()
+    frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # オプティカル・フローを計算
+    p1, st, err = cv2.calcOpticalFlowPyrLK(
+        old_gray, frame_gray, p0, None, **lk_params)
+
+    # 良い特徴点を選択
+    good_new = p1[st == 1]
+    good_old = p0[st == 1]
+
+    # 物体追跡を描画
+    for i, (new, old) in enumerate(zip(good_new, good_old)):
+        a, b = new.ravel()
+        c, d = old.ravel()
+        mask = cv2.line(mask, (a, b), (c, d), color[i].tolist(), 2)
+        frame = cv2.circle(frame, (a, b), 5, color[i].tolist(), -1)
+    img = cv2.add(frame, mask)
+
+    cv2.imshow('frame', img)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+
+    # ここで「前の」フレームと特徴点を更新
+    old_gray = frame_gray.copy()
+    p0 = good_new.reshape(-1, 1, 2)
+
+cv2.destroyAllWindows()
+cap.release()
